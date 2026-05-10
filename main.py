b61bdd7721e99221f570d200ae196ffe66733fc6
Alvin Lu <alvin.hk.lu@gmail.com>
2026-05-09 21:06:12 -0400

    asdasdasd

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 import sys
 import math
-from plant import Pot
+import random
+from plant import Pot, Venus, Cactus, Flower
+from pygame.locals import USEREVENT
 
 pygame.init()
 pygame.display.set_caption("Game of the Century")
@@ -14,6 +16,7 @@ from pygame.locals import (
     K_ESCAPE, # escape
     KEYDOWN, # key is pressed down
     QUIT, # X button pressed
+    MOUSEBUTTONDOWN,
 )
 
 class Hand:
@@ -23,11 +26,14 @@ class Hand:
         self.pos = pygame.Vector2(pygame.mouse.get_pos())
 
         # Hand Image
-        self.image = pygame.image.load("assets\hands0.png")
-        self.image = pygame.transform.scale(self.image, (150, 150))
+        self.image = pygame.image.load("assets\hands1.png")
+        self.image = pygame.transform.scale(self.image, (75, 75))
 
     def update(self):
         self.pos.xy = pygame.mouse.get_pos()
+
+    def addSatisfication(self, added):
+        self.satisfication = min(100, self.satisfication + added)
 
     """
     def handVisualChange(stage):
@@ -44,22 +50,38 @@ def blit_images(window):
     screen.blit(stats_panel, (0, 400))
     screen.blit(Pot1, )
 """
+
 # constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+background = pygame.transform.scale(
+    pygame.image.load(r"assets\spaceship_bg.png").convert(),
+    (SCREEN_WIDTH, SCREEN_HEIGHT),
+)
+
+
 table = pygame.transform.scale(pygame.image.load(r"assets\table.png"), (800, 200))
 
-
 hand = Hand((50), 0)
+
+# Initalizing pots
 pot1 = Pot(0, 1)
 pot2 = Pot(0, 2)
 pot3 = Pot(0, 3)
+pot_list = [[pot1, False, 0], [pot2, False, 1], [pot3, False, 2]]
+plant_list = ["", "", ""] # Placeholder for numbers
 
 pygame.mouse.set_visible(False)
 
+# Timers
+clock = pygame.time.Clock
+# Use a subtype so it doesn't collide with other USEREVENT uses
+GROWTH_TICK = USEREVENT + 1
+
+pygame.time.set_timer(GROWTH_TICK, 500)  # every 1000 ms push one event
 
 running = True
 while running:
@@ -73,17 +95,67 @@ while running:
         # The User closed the window
         elif event.type == QUIT:  
             running = False
-            
+
+        # Changes when plants are grown
+        elif event.type == GROWTH_TICK:
+            for plant in plant_list:
+                growthRng = random.randint(1, 3)
+                if plant != "" and growthRng == 1: # See if there is a plant or not
+                    plant.stageChange()
+
+        # Mouse Clicks
+        elif event.type == MOUSEBUTTONDOWN:
+            # Left Clicks
+            if event.button == 1:
+                for pot in pot_list:
+                    # Hand is touching pot
+                    if pot[0].rect.collidepoint(pygame.mouse.get_pos()) and not pot[1]:
+                        plant = random.randint(1, 3)
+                        pot_number = pot[2]
+                        pot[1] = True
+
+                        # Add venus
+                        if plant == 1:
+                            new_plant = Venus(0, pot_number + 1)
+                            plant_list[pot_number] = new_plant
+                            
+                        # Add Cactus
+                        elif plant == 2:
+                            new_plant = Cactus(0, pot_number + 1)
+                            plant_list[pot_number] = new_plant
+
+                        # Add flower
+                        else:
+                            new_plant = Flower(0, pot_number + 1)
+                            plant_list[pot_number] = new_plant
+
+                # Harvesting plants
+                for plant in plant_list:
+                    if plant != "" and plant.stage == 2:
+                        # Hand is touching plant
+                        if plant.rect.collidepoint(pygame.mouse.get_pos()):
+                            plant_list[plant.potNumber - 1] = "" # Removes plant from list
+                            plant.kill()
+                            hand.scareMeter += 1
+                    
     
-    screen.fill((0, 0, 0))
-    hand.update()
+    screen.blit(background, (0, 0))
+
     
     screen.blit(table, (0, 400))
-    screen.blit(pot1.image, [pot1.rect.x, pot1.rect.bottom])
-    screen.blit(pot2.image, [pot2.rect.x, pot2.rect.bottom])
-    screen.blit(pot3.image, [pot3.rect.x, pot3.rect.bottom])
-    screen.blit(hand.image, hand.pos)
 
+    # Displays Pots
+    for pot in pot_list:
+        screen.blit(pot[0].image, [pot[0].rect.x, pot[0].rect.bottom])
+    
+
+    # Displays plants
+    for plant in plant_list:
+        if plant != "":
+            screen.blit(plant.image, [plant.rect.x, plant.rect.bottom]) 
+
+    hand.update()
+    screen.blit(hand.image, hand.pos)
     pygame.display.flip()
     
 pygame.quit()
